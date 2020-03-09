e83862f1bb809a8dee9465a4daf57beecd920a54
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-03-09 14:28:08 +0800

    Update script.py
    Ready for wednesday discussion

--- Smart Washing Machine Simulator/script.py
+++ Smart Washing Machine Simulator/script.py
@@ -7,6 +7,10 @@
 import json 
 import requests
 
+#Import timed function execution functionality 
+import threading 
+from random import randrange
+
 #Store locations into an array
 locations = ["Novena","Balestier","Bukit Panjang"]
 
@@ -17,9 +21,33 @@ user_command = input("What do you want me to do? ")
 
 #Function to send message 
 def send(payload):
-        r = requests.post('https://reqres.in/api/users?page=2',json=payload)
+        r = requests.put('https://reqres.in/api/users?page=2',json=payload)
         print(r.text)
 
+#Function to simulate user 
+def simulate_user():
+    time = randrange(1,21)
+    threading.Timer(time,simulate_user).start()
+    random_machine_ID = randrange(1,19)
+    payload = {
+        "MachineID":random_machine_ID,
+        "errcode":"none",
+        "statuscode": "on"
+    }
+    send(payload)
+
+#Function to simulate random errors 
+
+def simulate_error():
+    time = randrange(1,121)
+    threading.Timer(time,simulate_user).start()
+    random_machine_ID = randrange(1,19)
+    payload = {
+        "MachineID":random_machine_ID,
+        "errcode":"simulated",
+        "statuscode": "broken"
+    }
+    send(payload)
 
 #Functionality 1: Be able to set all washers to work 
 #The command is "turn on all"
@@ -31,13 +59,30 @@ if user_command == "turn on all":
         #generate JSON message
         payload = {
             "MachineID":i,
-            "Status": "on"}
+            "errcode":"none",
+            "statuscode": "on"
+            }
         
         #Send JSON message 
         
         send(payload)
+
+#Functionality 2: Be able to turn off all washers 
+
+if user_command == "turn off all":
+    #Loop all locations to turn on all machines 
+    for i in range(1,number_of_washers+1):
         
-        # Uncomment below when the you know where to send the details to 
+        #generate JSON message
+        payload = {
+            "MachineID":i,
+            "errcode":"none",
+            "statuscode": "off"
+            }
+        
+        #Send JSON message 
+        
+        send(payload)
 
 #Functionality 2: Be able to set certain washers to turn on and off 
 #Command : modify
@@ -60,9 +105,20 @@ if user_command == "modify":
         status = input("Please specify what modification you want to make (on,off or broken)?")
     
     payload = {
-            "MachineID": machine_id,
-            "Status": status}
+            "MachineID":i,
+            "errcode":"simulated",
+            "statuscode": "broken"
+    }
     
     send(payload)
     
+#Functionality 4: Simulate multiple users and breakdowns 
+#Command: simulate 
+#Parameters : 
+# - breakdowns only happen once every 2 minutes or less to any machine 
+# - new request to use machine occurs once every 20 seconds or less 
+
+if user_command == "simulate":
+    simulate_user()
+    simulate_error()
 
