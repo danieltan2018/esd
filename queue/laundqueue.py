2599f9d9e52014fcb47df93cac17c62322af266a
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-03-28 12:24:55 +0800

    Update laundqueue.py

--- queue/laundqueue.py
+++ queue/laundqueue.py
@@ -67,7 +67,7 @@ def insert_queue(user_id, queue_id, location, date_time, service_type):
     #laundqueue = LaundQueue.query.filter_by(location=location, user_id = user_id).all()
     if(LaundQueue.query.filter_by(queue_id = queue_id, location = location).first()):
         code = 400
-        result = {"code": code,"message": "Queue ID Already Exist"}
+        result = {"code": code,"message": "Queue ID Already Exists"}
     data = request.get_json()
     laundqueue = LaundQueue.query.filter_by(location=location).all()
     # if(LaundQueue.query.filter_by(location=location).first()):
