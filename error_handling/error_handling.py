ff4e967f694416b75f9ea8331374be9a96edff21
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-03-31 14:54:06 +0800

    Update error_handling.py

--- error_handling/error_handling.py
+++ error_handling/error_handling.py
@@ -2,35 +2,45 @@ import json
 import sys
 import os
 import pika
+import requests
 
 MAILGUNKEY = os.getenv('MAILGUNKEY')
 
 hostname = "rabbit.delaundro.me"
 port = 5672
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=hostname, port=port))
 channel = connection.channel()
 exchangename = "laundro_topic"
 channel.exchange_declare(exchange=exchangename, exchange_type='topic')
 
+
 def receiveError():
     # prepare a queue for receiving messages
-    channelqueue = channel.queue_declare(queue="errorhandler", durable=True) # 'durable' makes the queue survive broker restarts
+    # 'durable' makes the queue survive broker restarts
+    channelqueue = channel.queue_declare(queue="errorhandler", durable=True)
     queue_name = channelqueue.method.queue
-    channel.queue_bind(exchange=exchangename, queue="queue_name", routing_key='*.error') 
+    channel.queue_bind(exchange=exchangename,
+                       queue="queue_name", routing_key='*.error')
 
     # set up a consumer and start to wait for coming messages
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-    channel.start_consuming() 
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
 
-def callback(channel, method, properties, body): # required signature for the callback; no return
+
+# required signature for the callback; no return
+def callback(channel, method, properties, body):
     print("Received an error by " + __file__)
     processError(json.loads(body))
     sendMessage(json.loads(body))
     print()
 
+
 def processError(order):
-    print("Recording an error:")    
+    print("Recording an error:")
     print(order)
+
 
 def sendMessage(order):
     return requests.post(
@@ -39,9 +49,8 @@ def sendMessage(order):
         data={"from": "DeLaundro <do_not_reply@delaundro.me>",
               "to": ["admin@delaundro.me"],
               "subject": "Machine Error",
-              "text": "Error: "+ order +" observed, please fix asap",
+              "text": "Error: " + order + " observed, please fix asap",
               "o:tracking": False})
 
-if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')
-    print("This is " + os.path.basename(__file__) + ": recording an error...")
-    
+
+receiveError()
