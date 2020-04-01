39de976a268473f21daa01312cb35af2a4a7ea5a
Edward-2018 <54498533+Edward-2018@users.noreply.github.com>
2020-04-01 20:13:41 +0800

    Update error_handling.py

--- error_handling/error_handling.py
+++ error_handling/error_handling.py
@@ -43,7 +43,7 @@ def processError(order):
 
 
 def sendMessage(order):
-    message = "Machine:"+str(order['machineid'])+ " at "  str(order['location']) +  " having this error " + str(order['errcodeid'])"
+    message = "Machine:"+str(order['machineid'])+ " at " + str(order['location']) +  " having this error " + str(order['errcodeid'])"
     print(message)
     return requests.post(
         "https://api.mailgun.net/v3/delaundro.me/messages",
