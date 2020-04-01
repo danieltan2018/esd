97d473229ea83eb58126ea6d2a28deb37b15f429
Edward-2018 <54498533+Edward-2018@users.noreply.github.com>
2020-04-02 01:20:53 +0800

    Update status.py

--- status/status.py
+++ status/status.py
@@ -300,11 +300,7 @@ def send_status(status):
         channel.queue_declare(queue='errorhandler', durable=True)
         channel.queue_bind(exchange=exchangename, queue='errorhandler', routing_key='*.error')
         channel.basic_publish(exchange=exchangename, routing_key="machine.error", body=message,properties=pika.BasicProperties(delivery_mode = 2))
-    elif status["errcodeid"] != 'none' or status["errcodeid"] != 0:
-            #inform Error
-        channel.queue_declare(queue='errorhandler', durable=True)
-        channel.queue_bind(exchange=exchangename, queue='errorhandler', routing_key='*.error')
-        channel.basic_publish(exchange=exchangename, routing_key="machine.error", body=message,properties=pika.BasicProperties(delivery_mode = 2))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, threaded=True)
