e6cc1759a2a3a30e311aa03fc30820c2600126ff
Edward-2018 <54498533+Edward-2018@users.noreply.github.com>
2020-04-01 19:42:22 +0800

    Update status.py

--- status/status.py
+++ status/status.py
@@ -294,7 +294,14 @@ def send_status(status):
     channel.basic_publish(exchange=exchangename, routing_key="machine.status",
                               body=message, properties=pika.BasicProperties(delivery_mode=2))
 
-    if status["errcodeid"] != 'none':
+    if "code" in status:
+        # inform Error
+        channel.queue_declare(queue='errorhandler', durable=True)
+        channel.queue_bind(exchange=exchangename,
+                           queue='errorhandler', routing_key='*.error')
+        channel.basic_publish(exchange=exchangename, routing_key="machine.error",
+                              body=message, properties=pika.BasicProperties(delivery_mode=2))
+    elif status["errcodeid"] != 'none':
         # inform Error
         channel.queue_declare(queue='errorhandler', durable=True)
         channel.queue_bind(exchange=exchangename,
