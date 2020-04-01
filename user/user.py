58d43bc1b40355e3b77622e6b1c2fc2f9e64448c
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-04-01 18:36:35 +0800

    Update dequeue

--- user/user.py
+++ user/user.py
@@ -76,6 +76,9 @@ def amqpcallback(channel, method, properties, body):
     if statuscode == 1:
         if id:
             send(id, "*Your wash has started!*\nWe will notify you when it's done.", [])
+            params = {'user_id' = id, 'location': status['location']}
+            url = QUEUEURL + 'serviceDequeue'
+            nextuser = requests.get(url=url, params=params)
     elif statuscode == 0:
         if id:
             send(id, "*Wash Complete!*\nPlease collect your laundry within 15 minutes.", [])
