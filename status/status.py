dbe3f3b0910f2996f29571b475ade923ecd36ccf
Edward-2018 <54498533+Edward-2018@users.noreply.github.com>
2020-04-01 20:34:52 +0800

    Update status.py

--- status/status.py
+++ status/status.py
@@ -224,6 +224,16 @@ def update_machine_User():
         result = {"code": code, "message": "Error Updating Data"}
 
     if code == 200:
+        if errcodeid == 0:
+            status.errcodeid = "none"
+        elif errcodeid == 1:
+            status.errcodeid = "Low on Detergent"
+        elif errcodeid == 2:
+            status.errcodeid = "Machine is Down"
+        elif errcodeid == 3:
+            status.errcodeid = "Water is Low"
+        else:
+            status.errcodeid = "Error Unknown Call Tier 3 Support"
         result = status.json()
     return str(result), code
 
