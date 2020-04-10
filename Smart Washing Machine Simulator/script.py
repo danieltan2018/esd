a280222a5438000b4e9fdbbfd56dbe9f5f26f21a
MatthewTan96 <matthew.tan.2018@sis.smu.edu.sg>
2020-04-10 18:15:06 +0800

    Update script.py

--- Smart Washing Machine Simulator/script.py
+++ Smart Washing Machine Simulator/script.py
@@ -11,13 +11,6 @@ import requests
 locations = ["Novena","Balestier","Bukit Panjang"]
 
 
-# def update(payload,URL):
-#     sendURL = "http://status.delaundro.me:8002/" + str(URL)
-#     print(payload)
-#     print(sendURL)
-#     r = requests.put(sendURL,json=payload)
-#     print(r.text)
-
 # Choose which functionality you want to enable 
 user_command = input("What do you want me to do? ")
 
@@ -117,71 +110,4 @@ if user_command == "turn off one machine":
     # print(change_status_url)
     # print(x.text)
 
-# Functionality 6: Create error 
 
-# #Functionality 2: Populate and turn off all washers 
-
-# if user_command == "turn off all":
-
-#     # Loop all identifiers to involve all machines  
-
-#     for location in locations:
-#         for i in range(1,7):
-#             #generate JSON message
-#             payload = {
-#                 "curuser": 1,
-#                 "errcodeid": 1,
-#                 "location": location,
-#                 "prevuser": 1,
-#                 "startcode": "none",
-#                 "statuscodeid": 1,
-#                 "unlockcode": "none"
-#             }
-            
-#             #Send JSON message 
-#             URL = str(i) + "&" + location
-#             create(payload,URL)
-
-# #Functionality 3: Be able to set certain washers to turn on and off 
-# #Command : modify
-
-# if user_command == "modify":
-#     machine_id = int(input("Please specify the machine ID "))
-    
-#     #Error validation: ID cannot be more than 6 
-#     while machine_id> 6 and machine_id<0:
-#         print("This is not a correct machine ID. We only have machine IDs to 6 in each location. Please try again")
-#         machine_id = int(input("Please specify the machine ID "))
-
-#     user_location = input("Please specify which location you want to make the change in (no caps)")
-#     #Error validation. Make sure that the location is specified to one of our branches 
-
-#     while user_location not in locations:
-#         print("We do not have a washing branch in that location. We only have it in novena, balestier or bukit panjang. Please try again")
-#         user_location = input("Please specify which location you want to make the change in (no caps)")
-
-#     statuscodeid = input("Please specify what modification you want to make (0 - available 1- unavailable 2- Error)? ")
-
-#     #Error validation
-#     states = ["0","1","2"]
-#     while statuscodeid not in states:
-#         print("This is not a correct modification. Please try again")
-#         statuscodeid = input("Please specify what modification you want to make (0 - available 1- unavailable 2- Error)? ")
-    
-
-#     payload = {
-#         "curuser": 1,
-#         "errcodeid": 1,
-#         "location": user_location,
-#         "machine_id":int(machine_id),
-#         "prevuser": 1,
-#         "startcode": "none",
-#         "statuscodeid": int(statuscodeid),
-#         "unlockcode": "none"
-#         }
-
-        
-#     URL = str(machine_id) + "&" + user_location
-    
-#     update(payload,URL)
-    
